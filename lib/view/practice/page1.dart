d08b8185ba8491136955db5f267391311761494a
usman <usman.mushtaq848@gmail.com>
2022-08-06 19:23:08 +0300

    expand and collapsed

--- lib/view/practice/page1.dart
+++ lib/view/practice/page1.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm_practice/view/topheadingcomponants/heading_componants.dart';
-
-class Page1 extends StatelessWidget {
+class Page1 extends StatefulWidget {
   const Page1({Key? key}) : super(key: key);
 
+  @override
+  State<Page1> createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+ late bool isExpand = true;
+  @override
+  void initState() {
+      super.initState();
+     isExpand = true;
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Column(
           children: [
-            SizedBox(height: 30),
-            HeadingImage(title: "Title", subTitle: "Lorum is the sub title of the text below", isImage: true, path: "assets/svg/heart_icon.svg",)
+          Container(
+            height: 70,
+            decoration: BoxDecoration(
+              color: isExpand == true?Colors.grey: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [ Row( children: [Icon( isExpand==true?Icons.file_copy: Icons.copy_all), SizedBox(width: 20), Text("Title Icons")]),IconButton(onPressed: (){
+                setState(() {
+                  isExpand =! isExpand;
+                });
+              }, icon: Icon(isExpand==true?Icons.arrow_back:Icons.arrow_back_ios))],
+            ),
+          ),
+         if(isExpand) Container(
+            height: 300,
+            color: Colors.green,
+          )
+         else Container(),
           ],
         ),
       ),
